49ca11497180a6c54e60a6bfd3b4c4ea63ce2eaa
xiren <xiren_88@126.com>
2015-07-30 23:27:59 +0800

    添加log和cluster配置

--- app.js
+++ app.js
@@ -1,39 +1,60 @@
-
 /**
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes');
+var express = require('express'),
+  routes = require('./routes');
 
 var MongoStore = require('connect-mongo')(express);
 var settings = require('./settings');
+var fs = require('fs');
+var accessLogfile = fs.createWriteStream('access.log', {
+  flags: 'a'
+});
+var errorLogfile = fs.createWriteStream('error.log', {
+  flags: 'a'
+});
 
 var app = module.exports = express.createServer();
 
+
+
 // Configuration
 
-app.configure(function(){
+app.configure(function() {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
   app.use(express.session({
-    secret : settings.cookieSecret,
-    store : new MongoStore({
+    secret: settings.cookieSecret,
+    store: new MongoStore({
       db: settings.db
     })
   }));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
+  app.use(express.logger({
+    stream: accessLogfile
+  }));
+  app.configure('production', function() {
+    app.error(function(err, req, res, next) {
+      var meta = '[' + new Date() + ']' + req.url + '\n';
+      errorLogfile.write(meta + err.stack + '\n');
+      next();
+    })
+  });
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+app.configure('development', function() {
+  app.use(express.errorHandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
 });
 
-app.configure('production', function(){
+app.configure('production', function() {
   app.use(express.errorHandler());
 });
 
@@ -62,25 +83,27 @@ app.get('/logout', routes.checkLogin);
 app.get('/logout', routes.logout);
 
 app.dynamicHelpers({
-  user:function(req, res){
+  user: function(req, res) {
     return req.session.user;
   },
-  error:function(req, res){
+  error: function(req, res) {
     var err = req.flash('error');
-    if(err.length)
+    if (err.length)
       return err;
     else
       return null;
   },
-  success:function(req, res){
+  success: function(req, res) {
     var succ = req.flash('success');
-    if(succ.length)
+    if (succ.length)
       return succ;
     else
       return null;
   }
 });
 
-app.listen(3000, function(){
+if (!module.parent) {
+app.listen(3000, function() {
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
+};
